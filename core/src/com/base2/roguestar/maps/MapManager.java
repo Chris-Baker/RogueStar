3eee5fa594df82cc65e41a373f01d58b21fc4b94
Chris Baker <chris@cbaker.co.uk>
2017-05-16 20:02:05 +0100

    Separate map loader and map renderer

--- core/src/com/base2/roguestar/maps/MapManager.java
+++ core/src/com/base2/roguestar/maps/MapManager.java
@@ -1,6 +1,5 @@
 package com.base2.roguestar.maps;
 
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 
@@ -11,24 +10,13 @@ public class MapManager {
 
     private TmxMapLoader loader;
     public TiledMap map;
-    //private OrthogonalTiledMapRenderer renderer;
 
     public void init() {
-
         loader = new TmxMapLoader();
         map = loader.load("maps/map.tmx");
-
-        //renderer = new OrthogonalTiledMapRenderer(map);
-    }
-
-    public void render(OrthographicCamera camera) {
-
-        //renderer.setView(camera);
-       // renderer.render();
     }
 
     public void dispose() {
         map.dispose();
-        //renderer.dispose();
     }
 }
