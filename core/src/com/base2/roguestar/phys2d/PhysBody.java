c1c00e2200bb0798c4330d85abeb3905e03aa8ef
Chris Baker <chris@cbaker.co.uk>
2017-06-08 00:06:07 +0100

    Stub out physics wrapper for collision geometry

---
+++ core/src/com/base2/roguestar/phys2d/PhysBody.java
@@ -0,0 +1,43 @@
+package com.base2.roguestar.phys2d;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+public class PhysBody {
+
+    private Array<PhysFixture> fixtures = new Array<PhysFixture>();
+    private PhysBodyType type = PhysBodyType.STATIC;
+    private Vector2 position = new Vector2();
+
+    public Array<PhysFixture> getFixtures() {
+        return fixtures;
+    }
+
+    public void addFixture(PhysFixture fixture) {
+        this.fixtures.add(fixture);
+    }
+
+    public PhysBodyType getType() {
+        return type;
+    }
+
+    public void setType(PhysBodyType type) {
+        this.type = type;
+    }
+
+    public float getX() {
+        return position.x;
+    }
+
+    public float getY() {
+        return position.y;
+    }
+
+    public void setPosition(float x, float y) {
+        this.position.set(x, y);
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position.set(position);
+    }
+}
---
+++ core/src/com/base2/roguestar/phys2d/PhysWorld.java
@@ -0,0 +1,7 @@
+package com.base2.roguestar.phys2d;
+
+import com.badlogic.gdx.utils.Array;
+
+public class PhysWorld {
+    private Array<PhysBody> bodies = new Array<PhysBody>();
+}
