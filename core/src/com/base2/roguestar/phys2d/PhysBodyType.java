b79d0973757e6a6b6a66a5bf5c3ec916311157b2
Chris Baker <chris@cbaker.co.uk>
2017-06-08 00:06:07 +0100

    Stub out physics wrapper for collision geometry

---
+++ core/src/com/base2/roguestar/phys2d/PhysBodyType.java
@@ -0,0 +1,6 @@
+package com.base2.roguestar.phys2d;
+
+public enum PhysBodyType {
+    STATIC,
+    KINEMATIC
+}
---
+++ core/src/com/base2/roguestar/phys2d/PhysFixture.java
@@ -0,0 +1,10 @@
+package com.base2.roguestar.phys2d;
+
+import com.badlogic.gdx.math.Shape2D;
+import com.badlogic.gdx.math.Vector2;
+
+public class PhysFixture {
+
+    private Shape2D shape;
+    private Vector2 position;
+}
