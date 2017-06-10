3319c9b42116679230f22e53b86d22e47875c896
Chris Baker <chris@cbaker.co.uk>
2017-06-10 12:12:06 +0100

    Update player phys2D body from movement system and with verified server updates

--- core/src/com/base2/roguestar/entities/systems/MovementSystem.java
+++ core/src/com/base2/roguestar/entities/systems/MovementSystem.java
@@ -12,22 +12,16 @@ import com.base2.roguestar.controllers.KeyboardController;
 import com.base2.roguestar.entities.components.ControllerComponent;
 import com.base2.roguestar.entities.components.CharacterComponent;
 import com.base2.roguestar.entities.components.RunSpeedComponent;
+import com.base2.roguestar.phys2d.PhysBody;
 
 /**
  * Created by Chris on 28/03/2016.
  */
 public class MovementSystem extends IteratingSystem {
 
-    private final float MAX_VELOCITY = 14.0f;
-
     private ComponentMapper<ControllerComponent> cm;
     private ComponentMapper<CharacterComponent> pm;
     private ComponentMapper<RunSpeedComponent> rm;
-
-    private final Vector2 velocity = new Vector2();
-
-    private float stillTime = 0;
-    private long lastGroundTime = 0;
 
     public MovementSystem() {
         super(Family.all(ControllerComponent.class, CharacterComponent.class, RunSpeedComponent.class).get());
@@ -46,91 +40,29 @@ public class MovementSystem extends IteratingSystem {
 
         CharacterController controller = c.controller;
         Body body = p.body;
-        Fixture physicsFixture = p.physicsFixture;
-        Fixture sensorFixture = p.sensorFixture;
+        PhysBody physBody = p.physBody;
         int runSpeed = r.runSpeed;
-
-        Vector2 vel = body.getLinearVelocity();
-        Vector2 pos = body.getPosition();
         float angle = body.getAngle();
+        float x = physBody.getX();
+        float y = physBody.getY();
 
-//        if (controller instanceof KeyboardController) {
-//            return;
-//        }
-
-        //System.out.println(pos.x + ", " + pos.y);
-
-        //System.out.println(p.isGrounded);
-
-//        if (p.isGrounded) {
-//            lastGroundTime = System.nanoTime();
-//        } else {
-//            if (System.nanoTime() - lastGroundTime < 100000000) {
-//                p.isGrounded = true;
-//            }
-//        }
-
-        body.setLinearVelocity(0, vel.y);
-
-        // cap max velocity on x
-//        if (Math.abs(vel.x) > MAX_VELOCITY) {
-//            vel.x = Math.signum(vel.x) * MAX_VELOCITY;
-//            body.setLinearVelocity(vel.x, vel.y);
-//        }
-
-        // calculate stilltime & damp
-        if (!controller.moveLeft && !controller.moveRight) {
-            stillTime += deltaTime;
-            //body.setLinearVelocity(vel.x * 0.9f, vel.y);
-        }
-        else {
-            stillTime = 0;
+        // move left
+        if (controller.moveLeft && !controller.moveRight) {
+            physBody.setPosition(x - (runSpeed * deltaTime), y);
+            body.setTransform(physBody.getX(), physBody.getY(), angle);
         }
 
-        // disable friction while jumping
-//        if (!p.isGrounded) {
-//            physicsFixture.setFriction(0f);
-//            sensorFixture.setFriction(0f);
-//        } else {
-//            if (!controller.moveLeft && !controller.moveRight && stillTime > 0.2) {
-//                physicsFixture.setFriction(100f);
-//                sensorFixture.setFriction(100f);
-//            }
-//            else {
-//                physicsFixture.setFriction(0.0f);
-//                sensorFixture.setFriction(0.0f);
-//            }
-//
-////            if(groundedPlatform != null && groundedPlatform.dist == 0) {
-////                player.applyLinearImpulse(0, -24, pos.x, pos.y,true);
-////            }
-//        }
-
-        // apply left impulse, but only if max velocity is not reached yet
-        if (controller.moveLeft && vel.x > -MAX_VELOCITY) {
-            //body.applyLinearImpulse(-10f, 0, pos.x, pos.y, true);
-            //body.setLinearVelocity(-15f, vel.y);
-            body.setTransform(pos.x - (10 * deltaTime), pos.y, angle);
-        }
-
-        // apply right impulse, but only if max velocity is not reached yet
-        if (controller.moveRight && vel.x < MAX_VELOCITY) {
-            //body.applyLinearImpulse(10f, 0, pos.x, pos.y, true);
-            //body.setLinearVelocity(15f, vel.y);
-            body.setTransform(pos.x + (10 * deltaTime), pos.y, angle);
+        // move right
+        if (controller.moveRight && !controller.moveLeft) {
+            physBody.setPosition(x + (runSpeed * deltaTime), y);
+            body.setTransform(physBody.getX(), physBody.getY(), angle);
         }
 
         // jump, but only when grounded
         if (controller.jump) {
-
-            System.out.println("Jumping");
-
             controller.jump = false;
-
             if (p.isGrounded) {
-                body.setLinearVelocity(vel.x, 0);
-                body.setTransform(pos.x, pos.y + 0.01f, 0);
-                body.applyLinearImpulse(0, 25, pos.x, pos.y, true);
+                // do the jump
                 p.isGrounded = false;
             }
         }
