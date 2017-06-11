1d9da36b259e84c0738aa3158d03462cb3c1a5df
Chris Baker <chris@cbaker.co.uk>
2017-06-11 23:23:14 +0100

    Add overlap checks to the Phys2D world and fixtures

--- core/src/com/base2/roguestar/phys2d/PhysFixture.java
+++ core/src/com/base2/roguestar/phys2d/PhysFixture.java
@@ -8,6 +8,12 @@ public class PhysFixture {
     private Shape2D shape;
     private Vector2 offset;
 
+    // tmp objects for overlap checks
+    private float[] rectangleVertices = new float[8];
+    Vector2 start = new Vector2();
+    Vector2 end = new Vector2();
+    Vector2 center = new Vector2();
+
     protected PhysFixture(PhysBody body) {
         this.body = body;
         this.offset = new Vector2();
@@ -103,4 +109,150 @@ public class PhysFixture {
         this.setPosition(position.x, position.y);
     }
 
+    public boolean overlaps(PhysFixture other) {
+
+        boolean overlaps = false;
+
+        if (shape instanceof Rectangle) {
+            Rectangle rectangle = (Rectangle)shape;
+            overlaps = overlaps(rectangle, other.getShape());
+        }
+        else if (shape instanceof Circle) {
+            Circle circle = (Circle)shape;
+            overlaps = overlaps(circle, other.getShape());
+        }
+        else if (shape instanceof Polygon) {
+            Polygon polygon = (Polygon)shape;
+            overlaps = overlaps(polygon, other.getShape());
+        }
+        else if (shape instanceof Polyline) {
+            Polyline polyline = (Polyline)shape;
+            overlaps = overlaps(polyline, other.getShape());
+        }
+
+        return overlaps;
+    }
+
+    private boolean overlaps(Rectangle rectangle, Shape2D other) {
+
+        boolean overlaps = false;
+
+        if (other instanceof Rectangle) {
+            overlaps = Intersector.overlaps((Rectangle)other, rectangle);
+        }
+        else if (other instanceof Circle) {
+            overlaps = Intersector.overlaps((Circle)other, rectangle);
+        }
+        else if (other instanceof Polygon) {
+            Polygon polygon = (Polygon)other;
+            overlaps = overlaps(polygon.getTransformedVertices(), rectangle);
+        }
+        else if (other instanceof Polyline) {
+            Polyline polyline = (Polyline)other;
+            overlaps = overlaps(polyline.getTransformedVertices(), rectangle);
+        }
+
+        return overlaps;
+    }
+
+    private boolean overlaps(Circle circle, Shape2D other) {
+
+        boolean overlaps = false;
+
+        if (other instanceof Rectangle) {
+            overlaps = Intersector.overlaps(circle, (Rectangle)other);
+        }
+        else if (other instanceof Circle) {
+            overlaps = Intersector.overlaps((Circle)other, circle);
+        }
+        else if (other instanceof Polygon) {
+            Polygon polygon = (Polygon)other;
+            overlaps = overlaps(polygon.getTransformedVertices(), circle);
+        }
+        else if (other instanceof Polyline) {
+            Polyline polyline = (Polyline)other;
+            overlaps = overlaps(polyline.getTransformedVertices(), circle);
+        }
+
+        return overlaps;
+    }
+
+    private boolean overlaps(Polygon polygon, Shape2D other) {
+
+        boolean overlaps = false;
+
+        if (other instanceof Rectangle) {
+            overlaps = overlaps(polygon.getTransformedVertices(), (Rectangle)other);
+        }
+        else if (other instanceof Circle) {
+            overlaps = overlaps(polygon.getTransformedVertices(), (Circle)other);
+        }
+        else if (other instanceof Polygon) {
+            overlaps = Intersector.overlapConvexPolygons(polygon, (Polygon)other);
+        }
+        else if (other instanceof Polyline) {
+            Polyline polyline = (Polyline)other;
+            overlaps = Intersector.overlapConvexPolygons(polygon.getTransformedVertices(), polyline.getTransformedVertices(), null);
+        }
+
+        return overlaps;
+    }
+
+    private boolean overlaps(Polyline polyline, Shape2D other) {
+
+        boolean overlaps = false;
+
+        if (other instanceof Rectangle) {
+            overlaps = overlaps(polyline.getTransformedVertices(), (Rectangle)other);
+        }
+        else if (other instanceof Circle) {
+            overlaps = overlaps(polyline.getTransformedVertices(), (Circle)other);
+        }
+        else if (other instanceof Polygon) {
+            Polygon polygon = (Polygon)other;
+            overlaps = Intersector.overlapConvexPolygons(polyline.getTransformedVertices(), polygon.getTransformedVertices(), null);
+        }
+        else if (other instanceof Polyline) {
+            overlaps = Intersector.overlapConvexPolygons(((Polyline)other).getTransformedVertices(), polyline.getTransformedVertices(), null);
+        }
+
+        return overlaps;
+    }
+
+    private boolean overlaps(float[] vertices, Circle circle) {
+
+        boolean overlaps = false;
+        float squareRadius = circle.radius * circle.radius;
+        center.set(circle.x, circle.y);
+
+        for (int i = 0, n = vertices.length; i < n && !overlaps; i += 2) {
+            overlaps = Intersector.intersectSegmentCircle(
+                    start.set(vertices[i - 2 % vertices.length], vertices[ i - 1 % vertices.length]),
+                    end.set(vertices[i], vertices[i + 1]),
+                    center,
+                    squareRadius);
+        }
+        return overlaps;
+    }
+
+    private boolean overlaps(float[] vertices, Rectangle rectangle) {
+
+        boolean overlaps = false;
+
+        rectangleVertices[0] = rectangle.getX();
+        rectangleVertices[1] = rectangle.getY();
+
+        rectangleVertices[2] = rectangle.getX();
+        rectangleVertices[3] = rectangle.getY() + rectangle.getHeight();
+
+        rectangleVertices[4] = rectangle.getX() + rectangle.getWidth();
+        rectangleVertices[5] = rectangle.getY() + rectangle.getHeight();
+
+        rectangleVertices[6] = rectangle.getX() + rectangle.getWidth();
+        rectangleVertices[7] = rectangle.getY();
+
+        overlaps = Intersector.overlapConvexPolygons(vertices, rectangleVertices, null);
+
+        return overlaps;
+    }
 }
--- core/src/com/base2/roguestar/phys2d/PhysWorld.java
+++ core/src/com/base2/roguestar/phys2d/PhysWorld.java
@@ -1,8 +1,11 @@
 package com.base2.roguestar.phys2d;
 
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.IntMap;
 
 public class PhysWorld {
+
+    private IntMap<PhysContact> contacts = new IntMap<PhysContact>();
     private Array<PhysBody> bodies = new Array<PhysBody>();
 
     public Array<PhysBody> getBodies() {
@@ -13,5 +16,32 @@ public class PhysWorld {
         PhysBody body = new PhysBody();
         this.bodies.add(body);
         return body;
+    }
+
+    public void overlaps(PhysFixture fixture, PhysFixture other) {
+
+        int hash = fixture.hashCode() ^ other.hashCode();
+
+        // check if there is already a contact
+        if (!contacts.containsKey(hash)) {
+
+            // check if these two fixtures overlap
+            if (fixture.overlaps(other)) {
+
+                // create the contact
+                PhysContact contact = new PhysContact(fixture, other);
+
+                // register a new contact
+                this.contacts.put(hash, contact);
+
+                // resolve contact
+                resolve(contact);
+            }
+        }
+    }
+
+    private void resolve(PhysContact contact) {
+        System.out.println("resolve contact");
+        this.contacts.remove(contact.hashCode());
     }
 }
